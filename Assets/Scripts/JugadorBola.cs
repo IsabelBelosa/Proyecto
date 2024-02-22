30b13241dcabb9e971e006e6bff6c08841059225
IsabelBelosa <isabel.becerralosada@alum.uca.es>
2024-02-22 14:03:41 +0100

    Avanzado Menu y casi acabado Nivel 1

--- Assets/Scripts/JugadorBola.cs
+++ Assets/Scripts/JugadorBola.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class JugadorBola : MonoBehaviour
 {
@@ -8,10 +10,14 @@ public class JugadorBola : MonoBehaviour
     public Camera camara;
     public GameObject suelo;
     public float velocidad=5;
+    public GameObject moneda;
+    public GameObject tronco;
+    public Text Contador;
 
     private Vector3 offset;
     private float ValX, ValZ;
     private Vector3 DireccionActual;
+    private int TotalMonedas = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -66,6 +72,20 @@ public class JugadorBola : MonoBehaviour
         suelo.gameObject.GetComponent<Rigidbody>().useGravity = true;
         yield return new WaitForSeconds(2);
         Destroy(suelo);
+
+        float ran = Random.Range(0f,1f);
+        if(ran < 1f) //Cada suelo que se genera tiene un 70% de posibilidades de poseer una moneda
+        {
+            ran = Random.Range(-2f,2f);
+            Instantiate(moneda,new Vector3(ValX + ran,1.5f,ValZ + ran), Quaternion.identity);
+        }
+        
+        float ran2 =  Random.Range(0f,1f);
+        if(ran2 < 0.3f) //Cada suelo que se genera tiene un 30% de posibilidades de poseer una moneda
+        { 
+            ran2 = Random.Range(-3f,3f);
+            Instantiate(tronco,new Vector3(ValX + ran2,0.5f,ValZ + ran2), Quaternion.identity);
+        }
     }
 
     void CambiartDireccion()
@@ -79,4 +99,23 @@ public class JugadorBola : MonoBehaviour
             DireccionActual = Vector3.forward;
         }
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.CompareTag("Premio"))
+       {
+        TotalMonedas++;
+        Contador.text = "Contador =" + TotalMonedas + "/7";
+        Destroy(other.gameObject);
+        if (TotalMonedas == 7)
+        {
+            SceneManager.LoadScene("2Nivel");
+        }
+       }
+       if(other.gameObject.CompareTag("tronco"))
+        {
+            Debug.Log("He chocado con un tronco");
+            SceneManager.LoadScene("HasPerdido");
+        }
+    } 
 }
